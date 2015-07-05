10a585f324663abdeb894e18726cbe14de004395
Raphi <raphi@infinitypixel.de>
2015-07-05 02:05:16 +0200

    Added: whitelisting / blacklisting channels + channels now specified on a multiline base

--- public/scripts/norecording.js
+++ public/scripts/norecording.js
@@ -31,12 +31,61 @@ registerPlugin({
             title: 'Used kick message',
             type: 'string',
             placeholder: 'No recording on our server!'
+        },
+        exemptChannels: {
+            title: 'Name-list of the channels which are exempted. (one per line)',
+            type: 'multiline'
+        },
+        whitelistOrBlacklist: {
+            title: 'Either allowing recording in the specified channels or not',
+            type: 'select',
+            options: [
+                'Allow',
+                'Deny'
+            ]
         }
     }
 }, function(sinusbot, config) {
-    if(config.kickMessage) {log('[No Rec.] Invalid kick message');return;}
-    sinusbot.on('record', function(ev) {
-        kickServer(ev.clientId, config.kickMessage);
+    if(!config.kickMessage) {log('[No Rec.] Invalid kick message');return;}
+    if(!config.whitelistOrBlacklist) {config.whitelistOrBlacklist = 0}
+    if(!config.exemptChannels) {config.exemptChannels = ""}
+    
+    var exemptedChannelNames = config.exemptChannels.split('\n').map(function(e) { 
+        return e.trim().replace(/\r/g, '');
     });
+    var exemptChannels = [];
+    
+    sinusbot.on('record', function(ev) {
+        var channels = getChannels();
+        var channel;
+        for (var i = 0; i < channels.length; i++) {
+            channel = channels[i];
+            if (exemptChannels.indexOf(channel.id) >= 0) {
+                if (config.whitelistOrBlacklist == 1){
+                    return;
+                }
+            }
+        }
+        kickServer(ev.clientId, config.kickMessage);
+        log('Client with id: ' + ev.clientId + ' tried to record. Kicking...');
+    });
+    
+    var updateChannels = function() {
+        log('[No Rec.] Connected, getting channels');
+        var channels = getChannels();
+        var channel_names = channels.map(function(e) { return e.name });
+        for(var i = 0; i < exemptedChannelNames.length; i++){
+            if(channel_names.indexOf(exemptedChannelNames[i]) >= 0){
+                var id = channels[channel_names.indexOf(exemptedChannelNames[i])].id;
+                if(exemptChannels.indexOf(id) == -1){
+                    exemptChannels.push(id);
+                }
+            }
+        }
+        log('[No Rec.] Exempted channels: ' + exemptChannels.toString());
+    };
+    
+    updateChannels();
+    sinusbot.on('connect', updateChannels);
 });
 
