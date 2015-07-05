d6ea3e48bfe4c975c99c016f038192ed2e5ab5eb
Raphi <raphi@infinitypixel.de>
2015-07-05 02:34:44 +0200

    Added: regex expressions + check channels on script load + ignored channel ids
    Now supports even regex expressions to find out channels.
    On script load, checks all channels for their names.
    The channels with the defined ids aren't deleted

--- public/scripts/badchan.js
+++ public/scripts/badchan.js
@@ -29,26 +29,77 @@ registerPlugin({
     author: 'Michael Friese <michael@sinusbot.com>, Raphael Touet <raphi@bypit.de>',
     vars: {
         names: {
-            title: 'Comma-separated list of forbidden names',
+            title: 'Comma-separated list of forbidden names or regex',
+            type: 'multiline'
+        },
+        ignoredChannels: {
+            title: 'Comma-separated list of ignored channel-ids',
             type: 'string'
         }
     }
 }, function(sinusbot, config) {
-    if (!config.names){
-        log('[BCN] Invalid channel names');
-        return;
+    if (!config.names) {log('[BCN] Invalid channel names'); return;}
+    var ignoredChannels = [];
+    if (config.ignoredChannels){
+        ignoredChannels = config.ignoredChannels.split('\n').map(function(e) { 
+            var n = parseInt(e.trim().replace(/\r/g, ''));
+            if(!n){
+                return -1;
+            }
+            return n;
+        }); 
     }
     
-    var names = config.names.split(',').map(function(e) { return e.trim() });
+    var names = config.names.split('\n').map(function(e) { return e.trim().replace(/\r/g, ''); });
+    
     sinusbot.on('channelCreate', function(ev) {
         if (!ev.name) return; // should not happen
+        if (ignoredChannels.indexOf(ev.id) >= 0) return;
         for (var i = 0; i < names.length; i++) {
-            if (ev.name.toLowerCase().indexOf(names[i].toLowerCase()) >= 0) {
-                log('Deleting channel ' + ev.name);
-                channelDelete(ev.id, true);
-                return;
+            if (names[i].match(/^\/.*\/$/)){
+                var reg = new RegExp(names[i]);
+                if(ev.name.match(reg)){
+                    log('Deleting channel ' + ev.name);
+                    channelDelete(ev.id, true);
+                    return;
+                }
+            } else {
+                if (ev.name.toLowerCase().indexOf(names[i].toLowerCase()) >= 0) {
+                    log('Deleting channel ' + ev.name);
+                    channelDelete(ev.id, true);
+                    return;
+                }
             }
         }
     });
+    
+    var updateChannels = function() {
+        var channels = getChannels();
+        var removed = [];
+        var channel;
+        for(var j = 0; j < channels.length; j++){
+            channel = channels[j];
+            if (ignoredChannels.indexOf(channel.id) >= 0) continue;
+            for (var i = 0; i < names.length; i++) {
+                if (names[i].match(/^\/.*\/$/)){
+                    var reg = new RegExp(names[i]);
+                    if(channel.name.match(reg)){
+                        removed.push(channel.name);
+                        channelDelete(channel.id, true);
+                    }
+                } else {
+                    if (channel.name.toLowerCase().indexOf(names[i].toLowerCase()) >= 0) {
+                        removed.push(channel.name);
+                        channelDelete(channel.id, true);
+                    }
+                }
+            }
+        }
+        log('[BCN] Removed following channels: ' + removed.toString());
+    };
+    
+    updateChannels();
+    sinusbot.on('connect', updateChannels);
+    
 });
 
