2072280b4bcc9d4985adffc4e937d105471cbf1e
Raphi <raphi@infinitypixel.de>
2015-07-08 19:35:51 +0200

    Fixed some issues

--- public/scripts/greetonjoin.js
+++ public/scripts/greetonjoin.js
@@ -22,7 +22,7 @@
  */
 registerPlugin({
     name: 'Join-Greeting',
-    version: '1.0',
+    version: '1.0 BETA',
     description: 'This plugin will let the bot greet everyone who joins the channel.',
     author: 'Raphael Touet <raphi@bypit.de>',
     vars: {
@@ -48,35 +48,41 @@ registerPlugin({
         }
     }
 }, function(sinusbot, config){
-    log('[J-Greet] Join-Greeting v1.0 by Raphael Touet');
+    log('[J-Greet] Join-Greeting v1.0 BETA by Raphael Touet');
     
     if(!config.message) {log('[J-Greet] Invalid message');return;}
     if(!config.type) {log('[J-Greet] Invalid type');return;}
-    if(!config.locale) {log('[J-Greet] Invalid locale');return;}
     
-    if(config.locale.match(/utf-8|^[a-z]{2}(?:-[a-z]{2})?$/i)) {log('[J-Greet] Invalid locale');return;}
+    var type = parseInt(config.type, 10);
     
-    sinusbot.on('clientJoin', function(ev){
-       var msg = config.message;
-       msg = msg.replace(/%n/g, ev.clientNick);
-       if(config.type != 3){
-           msg = msg.replace(/(?:[url=.{1,}])?((https?:\/\/(?:www\.)?[a-zA-Z0-9._\/-]+\.[a-zA-Z]{2,63})([\/?\#](?:.){0,})?)(?:[/url])/gi, '[url=$1]$1[/url]');
-       } else {
-           msg = msg.replace(/\[.\](.{0,})\[\/.\]/i, '$1');
-           msg = msg.replace(/<.>(.{0,})<\/.>/i, '$1');
-       }
+    if(type == 3){
+        if(!config.locale) {log('[J-Greet] Invalid locale');return;}
+        if(!config.locale.match(/utf-8|^[a-z]{2}(?:-[a-z]{2})?$/i)) {log('[J-Greet] Invalid locale');return;}
+    }
+    
+    
+    sinusbot.on('clientJoin', function(ev){        
+        var msg = config.message;
+        msg = msg.replace(/%n/g, ev.clientNick);
+        if(type != 3){
+            msg = msg.replace(/(?:[url=.{1,}])?((https?:\/\/(?:www\.)?[a-zA-Z0-9._\/-]+\.[a-zA-Z]{2,63})([\/?\#](?:.){0,})?)(?:[/url])/gi, '[url=$1]$1[/url]');
+        } else {
+            msg = msg.replace(/\[.\](.{0,})\[\/.\]/i, '$1');
+            msg = msg.replace(/<.>(.{0,})<\/.>/i, '$1');
+        }
        
-       if(config.type == 0){
-           chatPrivate(ev.clientId, msg);
-       } else if(config.type == 1){
-           chatChannel(msg);
-       } else if(config.type == 2){
-           poke(ev.clientId, msg);
-       } else if(config.type == 3){
-           say(msg, config.locale);
-       }
+        if(type == 0){
+            chatPrivate(ev.clientId, msg);
+        } else if(type == 1){
+            log('channel');
+            chatChannel(msg);
+        } else if(type == 2){
+            poke(ev.clientId, msg);
+        } else if(type == 3){
+            say(msg, config.locale);
+        }
        
     });
     
-    log('[J-Greet] Initialized. Now works.');
+    log('[J-Greet] Initialized.');
 });
