7f88fe031fb4562fd42e6b0223c3f9d5376e9a50
Raphi <raphi@bypit.de>
2015-09-02 12:32:15 +0200

    + imp: changed copyright e-mail adress!!

--- public/scripts/announcetrack.js
+++ public/scripts/announcetrack.js
@@ -1,5 +1,5 @@
 /* 
- * Copyright (C) 2015 Raphael Touet <raphi@bypit.de>
+ * Copyright (C) 2015 Raphael Touet <raphraph@raphraph.de>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
